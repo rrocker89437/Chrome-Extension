de64e274f30bcefd0e502f273f312add391c7941
rrocker89437 <ramiah.rocker@yahoo.com>
2025-01-30 15:57:22 -0800

    fixing bugs

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 let myLeads = [];
-const inputBtn = document.querySelector("#input-btn");
-const inputEl = document.querySelector("#input-el");
-const ulEl = document.querySelector("#ul-el");
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click", () => {
-  // Push the value from the inputEl into myLeads
-  myLeads.push(inputEl.value);
-  // Clear out the input field
-  renderLead();
-  inputEl.value = "";
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
 });
 
-function renderLead() {
-  // Wrap the lead in an anchor tag (<a>) inside the <li>
-  // Can you make the link open in a new tab?
-  let listItem = `
-  <li>
-    <a target='_blank' href='${inputEl.value}'>
-    ${inputEl.value}
-    </a>
-  </li>
-  `;
-  ulEl.innerHTML += listItem;
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
 
 // function renderLeads() {
@@ -39,3 +53,20 @@ function renderLead() {
 //}
 //     ulEl.innerHTML = listItems
 // }
+
+//-----------------------------------------------
+// store leads in local storage
+// let myLeads = `["www.awesomelead.com"]`;
+
+// 1. Turn the myLeads string into an array
+// myLeads = JSON.parse(myLeads);
+// 2. Push a new value to the array
+// myLeads.push("www.lead2.com");
+// 3. Turn the array into a string again
+// myLeads = JSON.stringify(myLeads);
+// 4. Console.log the string using typeof to verify that it's a string
+// console.log(typeof myLeads);
+
+// localStorage.clear();
+// let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// localStorage.setItem("myLeads", JSON.stringify(myLeads));
