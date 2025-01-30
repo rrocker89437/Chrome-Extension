d633a14ebde51d01edc64c503e2eb00235b83120
rrocker89437 <ramiah.rocker@yahoo.com>
2025-01-30 11:53:24 -0800

    setting up files

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+let myLeads = [];
+
+inputBtn.addEventListener("click", () => {
+  console.log("Button Clicked");
+});
