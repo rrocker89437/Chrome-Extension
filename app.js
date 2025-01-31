22ebd3f78e99ff635216be4ba9332f005d5d1db3
rrocker89437 <ramiah.rocker@yahoo.com>
2025-01-30 17:00:24 -0800

    Deploying extension

--- app.js
+++ app.js
@@ -1,42 +1,55 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
+// Save Leads in local Storage
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+// Render Leads in Extension
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `;
   }
   ulEl.innerHTML = listItems;
 }
+
+// Delete All
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+// Save Tab
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+// Save Input
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
 
 // function renderLeads() {
 // Create a variable, listItems
@@ -70,3 +83,22 @@ function renderLeads() {
 // localStorage.clear();
 // let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 // localStorage.setItem("myLeads", JSON.stringify(myLeads));
+//-----------------------------------------------
+//Parameters
+// function greetUser(greeting, name) {
+//   welcomeEl.textContent = `${greeting}, ${name} 👋`;
+// }
+//Arguments
+// greetUser("Howdy", "James");
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+// add(3, 4);
+//-----------------------------------------------
+// Create a function, getFirst(arr), that returns the first item in the array
+// function getFirst(arr) {
+//   return arr[0];
+// }
+// let f
+// console.log(firstCard);
